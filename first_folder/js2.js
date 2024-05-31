7d2806e6a33ef506a46dad3673b8a4d4c7b4e26f
sujitbarnawal <166618250+sujitbarnawal@users.noreply.github.com>
2024-05-31 12:01:05 +0000

    comparision of data types

---
+++ first_folder/js2.js
@@ -0,0 +1,17 @@
+// console.log(null>=0)
+// console.log(null<0)
+// console.log(null==0)
+
+// console.log(undefined>=0)
+// console.log(undefined==0)
+// console.log(undefined<0)
+
+// console.log("2"===2);
+// console.log("2"==2);
+
+
+// Primitive data types
+/* String,Number,Boolean,Symbol,BigInt,Null,Undefined */
+
+//Non  primitive data types
+/* arrays,Functions,Objects*/
