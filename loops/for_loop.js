245b31856fafddda6ab552d06a0e118095057ce7
sujitbarnawal <sujitbarnawal4545@gmail.com>
2024-06-03 16:58:15 +0545

    while_loops

---
+++ loops/for_loop.js
@@ -0,0 +1,32 @@
+// for loop
+
+for (let index = 0; index < 10; index++) {
+    // console.log(index+1); 
+}
+
+for(i=0;i<10;i++){
+    for(j=0;j<10;j++){
+        // console.log(`${i},${j}`);
+    }
+}
+
+let arr=[1,2,3,4,5]
+
+for (let i = 0; i < arr.length; i++) {
+    //  console.log(arr[i]**3);   
+}
+
+
+for (let index = 0; index < 5; index++) {
+    if(index==3){
+        break
+    }
+    // console.log(index)
+}
+
+for (let index = 0; index < 5; index++) {
+    if(index==3){
+        continue
+    }
+    // console.log(index)
+}
---
+++ loops/while_loop.js
@@ -0,0 +1,28 @@
+// let i=0;
+// while (i<10) {
+// //    console.log(i+1); 
+//    i++
+// }
+
+
+// let arr=[1,2,3]
+
+// let j=0
+
+// while(j<arr.length){
+//     console.log(arr[j]);
+//     j++
+// }
+
+// let k = 1
+// do {
+//     console.log(k);
+//     k++
+// } while (k <= 5);
+
+// let array=[1,2,3,4,5,6,7,8,9,10]
+// let m=0
+// do {
+//     console.log(array[m]);
+//     m++
+// } while (m<array.length);
