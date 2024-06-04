0c2eb84a6bdc8994a5d6c332745769c06edfa8db
sujitbarnawal <sujitbarnawal4545@gmail.com>
2024-06-04 10:42:44 +0545

    for in loop

---
+++ loops/for_in_loop.js
@@ -0,0 +1,20 @@
+let obj={
+    SAM:"Simulation and modeling",
+    SAD:"System Analysis and Design",
+    DAA:"Design of Analysis and Algorithm",
+    CGY:"Cryptography",
+    MM:"Multimedia",
+    WT:"Web Technology"
+}
+
+for (const key in obj) {
+    // console.log(key);
+    // console.log(obj[key]);
+    // console.log(key,obj[key]);
+}
+
+let arr=[1,2,3,4,5]
+
+for (const i in arr) {
+    // console.log(arr[i]);
+}
