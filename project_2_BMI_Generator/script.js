8b5ad9fae1ac9b34564aadf9f2ed8b556b13286b
sujitbarnawal <sujitbarnawal4545@gmail.com>
2024-06-05 10:25:42 +0545

    script file for BMI calculator

--- project_2_BMI_Generator/script.js
+++ project_2_BMI_Generator/script.js
@@ -1,18 +1,17 @@
 
 
-let height=document.querySelector("#height")
-let weight=document.querySelector("#weight")
-let result=document.querySelector("#result")
-let button = document.querySelector("button")
+let height = document.querySelector("#height");
+let weight = document.querySelector("#weight");
+let result = document.querySelector("#result");
+let button = document.querySelector("button");
 
-button.addEventListener("click",()=>{
-    let heightValue=parseFloat(height.value)
-    let weigthValue=parseFloat(weight.value)
-    let heightInMeters=heightValue/100
-    let bmi=weigthValue/(heightInMeters**2)
-    result.innerHTML=bmi.toFixed(2)+"kg/m<sup>2</sup>"
-    result.classList.remove("final")
-})
+button.addEventListener("click", () => {
+    let heightValue = parseFloat(height.value);
+    let weightValue = parseFloat(weight.value);
 
+    let heightInMeters = heightValue / 100;
+    let bmi = weightValue / (heightInMeters ** 2);
+    result.innerHTML = bmi.toFixed(2) + " kg/m<sup>2</sup>";
+    result.classList.remove("final");
 
-
+});
