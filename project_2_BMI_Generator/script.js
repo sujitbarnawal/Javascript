bdd7ba16d27ac2234c15259476414f066943ea44
sujitbarnawal <sujitbarnawal4545@gmail.com>
2024-06-05 10:32:51 +0545

    script file for BMI calculator

--- project_2_BMI_Generator/script.js
+++ project_2_BMI_Generator/script.js
@@ -9,9 +9,15 @@ button.addEventListener("click", () => {
     let heightValue = parseFloat(height.value);
     let weightValue = parseFloat(weight.value);
 
-    let heightInMeters = heightValue / 100;
-    let bmi = weightValue / (heightInMeters ** 2);
-    result.innerHTML = bmi.toFixed(2) + " kg/m<sup>2</sup>";
+    if (isNaN(heightValue) || heightValue <= 0) {
+        result.innerHTML = "Please enter a valid height";
+    } else if (isNaN(weightValue) || weightValue <= 0) {
+        result.innerHTML = "Please enter a valid weight";
+    } else {
+        let heightInMeters = heightValue / 100;
+        let bmi = weightValue / (heightInMeters ** 2);
+        result.innerHTML = bmi.toFixed(2) + " kg/m<sup>2</sup>";
+        
+    }
     result.classList.remove("final");
-
 });
