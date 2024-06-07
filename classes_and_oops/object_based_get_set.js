114fabc252267970109583c5e0063f1572d39bf5
sujitbarnawal <sujitbarnawal4545@gmail.com>
2024-06-07 12:11:25 +0545

    get set in object

---
+++ classes_and_oops/object_based_get_set.js
@@ -0,0 +1,16 @@
+const User = {
+    _email: 'sujit@gmail.com',
+    _password: "AbCdEfGh",
+
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+const user1 = Object.create(User)
+console.log(user1.email);
